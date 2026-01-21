c9a03e15396c10a0858cb943e1466cf3762ed606
naix1337 <florian150408@gmail.com>
2026-01-21 18:02:16 +0100

    Fix: ecosystem.config.js JavaScript module format

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -1,25 +1,25 @@
-{
-    "apps": [
+module.exports = {
+    apps: [
         {
-            "name": "keyauth-backend",
-            "script": "dist/main.js",
-            "instances": 2,
-            "exec_mode": "cluster",
-            "watch": false,
-            "max_memory_restart": "500M",
-            "env": {
-                "NODE_ENV": "production",
-                "PORT": 3000
+            name: "keyauth-backend",
+            script: "dist/main.js",
+            instances: 2,
+            exec_mode: "cluster",
+            watch: false,
+            max_memory_restart: "500M",
+            env: {
+                NODE_ENV: "production",
+                PORT: 3000
             },
-            "error_file": "/var/log/pm2/keyauth-backend-error.log",
-            "out_file": "/var/log/pm2/keyauth-backend-out.log",
-            "log_date_format": "YYYY-MM-DD HH:mm:ss Z",
-            "merge_logs": true,
-            "autorestart": true,
-            "max_restarts": 10,
-            "min_uptime": "10s",
-            "listen_timeout": 3000,
-            "kill_timeout": 5000
+            error_file: "/var/log/pm2/keyauth-backend-error.log",
+            out_file: "/var/log/pm2/keyauth-backend-out.log",
+            log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+            merge_logs: true,
+            autorestart: true,
+            max_restarts: 10,
+            min_uptime: "10s",
+            listen_timeout: 3000,
+            kill_timeout: 5000
         }
     ]
-}
+};
